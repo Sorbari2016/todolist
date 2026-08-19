32725ff89e53fd45fbd6dd13a83ac8d668277875
sorbari2016 <152311585+Sorbari2016@users.noreply.github.com>
2026-08-20 00:06:45 +0100

    Handle Sidebar button clicks
    -  Add event delegation for the 4 sidebar buttons.
    -  Adjust the renderMainArea method.
    -  Import icons.
    - Create getAddTaskForm to render form
    - Style form.

--- src/modules/dom.js
+++ src/modules/dom.js
@@ -1,12 +1,19 @@
 // DOM LOGIC, & METHODS
 
+// imports
+import priorityIcon from "../../assets/icons/priority_flag.png";
+import sunnyIcon from "../../assets/icons/sunny.png";
+import sortIcon from "../../assets/icons/arrow.png";
+import groupIcon from "../../assets/icons/group-icon.png";
+import calendarIcon from "../../assets/icons/calendar.png";
+import notificationIcon from "../../assets/icons/bell.png";
+import repeatIcon from "../../assets/icons/repeat.png";
+
 // Hamburger method
 // select elements
 const sidebar = document.getElementById("sidebar");
 const hamburger = document.getElementById("hamburger-btn");
 const overlay = document.querySelector(".overlay");
-
-console.log(sidebar, hamburger, overlay);
 
 // create handler to handle sidebar button click
 hamburger.addEventListener("click", () => {
@@ -31,13 +38,12 @@ function clearMainArea() {
 // Creea function to re-render main area
 function renderMainArea() {
   mainArea.innerHTML = `
-    <main id="main-area">
           <div class="toolbar">
             <div class="toolbar-top">
               <div class="toolbar-headline">
                 <div class="title">
                   <img
-                    src="../assets/icons/sunny.png"
+                    src="${sunnyIcon}"
                     alt="sunny day icon"
                   /><span>My Day</span>
                 </div>
@@ -47,14 +53,14 @@ function renderMainArea() {
                 <ul class="toolbar-nav">
                   <li class="item main-item">
                     <button id="sort" class="btn click">
-                      <img src="/assets/icons/arrow.png" alt="sort icon" />
+                      <img src="${sortIcon}" alt="sort icon" />
                       Sort
                     </button>
                   </li>
                   <li class="item main-item">
                     <button id="group" class="btn click">
                       <img
-                        src="../assets/icons/group-icon.png"
+                        src="${groupIcon}"
                         alt="group icon"
                       />
                       Group
@@ -82,28 +88,117 @@ function renderMainArea() {
                   <ul>
                     <li class="item main-item">
                       <img
-                        src="../assets/icons/calendar.png"
+                        src="${calendarIcon}"
                         alt="calender icon"
                       />
                     </li>
                     <li class="item main-item">
                       <img
-                        src="../assets/icons/bell.png"
+                        src="${notificationIcon}"
                         alt="notification icon"
                       />
                     </li>
                     <li class="item main-item">
-                      <img src="../assets/icons/repeat.png" alt="repeat icon" />
+                      <img src="${repeatIcon}" alt="repeat icon/>
                     </li>
                   </ul>
                 </div>
-                <button id="addBtn" type="submit">Add</button>
+                <button id="add-btn" type="submit">Add</button>
               </span>
             </div>
             <div class="added-task-list">
               <ul></ul>
             </div>
           </div>
-        </main>
   `;
+}
+
+// Add Event delegation on the sidebar top section
+[...document.getElementById("sidebar").children][0].addEventListener(
+  "click",
+  (e) => {
+    if (e.target.tagName === "BUTTON") {
+      const buttonID = e.target.getAttribute("id");
+
+      if (buttonID === "add-task-sidebar") {
+        getAddTaskForm();
+      }
+    }
+  },
+);
+
+// Create a method for the add task button
+function getAddTaskForm() {
+  // clear the main area
+  clearMainArea();
+
+  // create form element
+  const form = document.createElement("form");
+  form.setAttribute("id", "add-task-form");
+
+  // create form markup
+  form.innerHTML = `
+        <h2>Create a New Task</h2>
+        <div class="form-item">
+            <input type="checkbox" id="checkbox" name="checklist">
+            <input type="text" name="title" id="title" placeholder="Read for 3 hours..." required>
+        </div>
+        <div class="form-item">
+            <textarea name="desc" id="desc" placeholder="Describe your task"></textarea>
+        </div>
+        <div class="form-item">
+            <input type="date" id="due-date" name="dueDate">
+        </div>
+        <div class="form-item">
+            <input type="text" name="note" id="note" placeholder="Add a note...">
+        </div>
+        <div class="form-item">
+            <img src="${priorityIcon}" alt="priority icon">
+            <select name="priority" id="priority">
+                <option value="">--Select a priority level</option>
+                <option value="low">Low</option>
+                <option value="medium">Medium</option>
+                <option value="high">High</option>
+            </select>
+        </div>
+        <div class="form-actions">
+            <button  type = "button" class="cancel-btn">Cancel</a>
+            <button type = "button" class="add-task-btn">Add</button>
+        </div>
+    `;
+  // append to main area
+  mainArea.appendChild(form);
+
+  // add event handler to handle the button clicks
+  form.querySelectorAll("button").forEach((button) => {
+    button.addEventListener("click", () => {
+      const buttonClass = button.getAttribute("class");
+
+      // when the cancel btn is clicked
+      if (buttonClass.startsWith("cancel")) {
+        clearMainArea();
+        renderMainArea();
+      } else {
+        // handle submission
+        // disable checkbox
+        form.querySelector("#checkbox").disabled = true;
+
+        // get the other values
+        const title = form.querySelector("#title").value.trim();
+        const desc = form.querySelector("#desc").value.trim();
+        const dueDate = form.querySelector("#due-date").value.trim();
+        const note = form.querySelector("#note").value.trim();
+        const priorityLevel = form.querySelector("#priority").value.trim();
+
+        // ensure task has a title
+        if (!title) {
+          alert("Your task must have a title");
+          return;
+        }
+
+        // add task
+        // update local storage
+      }
+    });
+  });
 }
